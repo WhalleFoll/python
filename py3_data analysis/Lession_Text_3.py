6c660dabbc6bb9ead7bfac709db1ce438666b45c
Hear <2772950347@qq.com>
2022-03-27 14:16:10 +0800

    2023.3.27

--- py3_data analysis/Lession_Text_3.py
+++ py3_data analysis/Lession_Text_3.py
@@ -1,67 +1,4 @@
-#1
-# import numpy as np
-# x=np.array((1,2,3))
-# y=np.array((4,5,6))
-# print(np.dot(x,y))
-# print(x.dot(y))
-# print(sum(x*y))
-#
-# x=np.matrix([[1,2,3],[4,5,6]])
-# y=np.matrix([1,2,3,4,5,6])
-# print(x,y,x[1,1],sep='\n\n')
-#
-
-# 2
-# import  numpy as np
-# import random
-# data_zero=np.zeros((2,3))
-# data_one =np.ones((3,4))
-# data_random=np.random.randint(0,10,(3,2))
-# print(data_zero)
-# print(data_one)
-# print(data_random)
-
-# #t2
-# 1）a=np.array([[1,2,3],[4,5,6]])（查看数组的维度，数组元素的个数）。
-# 2）将a数组的行变列，返回最后一个元素，返回第2到第4个元素，返回逆序的数组
-# 3）a=np.arange(9).reshape(3,3)
-# b=np.arange(9).reshape(3,3)
-# 将a、b数组水平合并，垂直合并，深度合并
-# 4）将a数组水平拆分，垂直拆分，深度拆分
-# 5）数组运算（与常的四则运算，与数组的四则运算，判断数组是否相等）
-# a=np.arange(4,dtype=np.float32).reshape(2,2)
-# b=np.arange(4,8,dtype=np.float32).reshape(2,2)
-# 求a+2,a+b,a/b,a*b,判断数组a,b是否相等
-# 6）对数组a求和、积、平均值、最大值、最小值、元素替换、方差、标准差
-# print(f'税前薪资是：{salary}元， 缴税：{tax}元， 税后薪资是：{aftertax}元')
-import  numpy as np
-
-a=np.array([[1,2,3],[4,5,6]])
-
-print(f'维度：{a.shape} 元素个数：{a.size}')
-a_trans = a.transpose()
-print(f'行列交换：{a_trans} ')
-
-a=np.arange(9).reshape(3,3)
-b=np.arange(9).reshape(3,3)
-print(f'a b垂直合并：\n{np.vstack((a, b))}\n'
-      f'a b水平合并：\n{np.hstack((a, b))}\n'
-      f'a b深度合并：\n{np.dstack((a,b))}\n')
-print('-----------------------------------')
-
-
-
-a=np.arange(4,dtype=np.float32).reshape(2,2)
-b=np.arange(4,8,dtype=np.float32).reshape(2,2)
-
-print(f'a+2:\n{a+2}\n'
-      f'a+b:\n{np.add(a,b)}\n'
-      f'a*b:\n{np.multiply(a,b)}\n'
-      f'a/b:\n{np.divide(a,b)}\n')
-
-
-
-#4.已知矩阵[[-1,1,0],[-4,3,0],[1,0,2]]，求矩阵的特征向量、特征值、矩阵的逆。
+# 4.已知矩阵[[-1,1,0],[-4,3,0],[1,0,2]]，求矩阵的特征向量、特征值、矩阵的逆。
 # import numpy as np
 # A=np.array([[-1,1,0],[-4,3,0],[1,0,2]])
 # print("原矩阵：\n",A)
@@ -135,4 +72,36 @@ print(f'a+2:\n{a+2}\n'
 #             "n_components =" + str(comps[i]))
 # plt.savefig('svd.jpg')
 # plt.show()
+
+# # 3. 利用Numpy产生正态分布图，理解下面代码，并给每行代码进行注释
+import numpy as np
+from numpy.linalg import cholesky
+import matplotlib.pyplot as plt
+sampleNo = 1000
+# 一维正态分布, 下面三种方式是等效的
+mu = 3
+sigma = 0.1
+np.random.seed(0)             # 使得随机数据可预测。
+s = np.random.normal(mu, sigma, sampleNo)  # 从正态（高斯）分布中抽取随机样本。
+plt.subplot(141)              # 表示把显示界面分割成1*4的网格。其中，第一个参数是行数，第二个参数是列数，第三个参数表示图形的标号。
+plt.hist(s, 30, normed=True)  # 绘制直方图  这个参数是指定每个bin(箱子)分布的数据,对应x轴  这个参数指定bin(箱子)的个数,也就是总共有几条条状图
+
+np.random.seed(0)
+s = sigma * np.random.randn(sampleNo) + mu
+plt.subplot(142)
+plt.hist(s, 30, normed=True)
+np.random.seed(0)
+s = sigma * np.random.standard_normal(sampleNo) + mu
+plt.subplot(143)
+plt.hist(s, 30, normed=True)
+
+# 二维正态分布
+mu = np.array([[1, 5]])
+Sigma = np.array([[1, 0.5], [1.5, 3]])
+R = cholesky(Sigma)  #
+s = np.dot(np.random.randn(sampleNo, 2), R) + mu
+plt.subplot(144)
+# 注意绘制的是散点图，而不是直方图
+plt.plot(s[:,0],s[:,1],'+')
+plt.show()
 
